b9305c96349ada0a5f0e34f4069072d28daaf6e5
anTIMattter <kedingt@gmail.com>
2016-02-02 21:34:25 -0600

    reformatting
    removed necessary apps, and added proper models.

--- apartment/apartmentApp/models.py
+++ apartment/apartmentApp/models.py
@@ -1,3 +1,30 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+class BulletinPost(models.Model):
+	post_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	posted_by = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.post_text
+
+class BulletinReply(models.Model):
+	reply_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	posted_by = models.CharField(max_length=100)
+	replied_to = models.ForeignKey(BulletinPost, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.reply_text
+
+class Message(models.Model):
+	message_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	sent_by = models.CharField(max_length=100)
+	sent_to = models.CharField(max_length=100)
+	urgency = models.IntegerField()
+
+	def __str__(self):
+		return self.message_text
