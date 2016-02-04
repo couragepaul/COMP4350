abd273c03d66c9a9a36160f0ba2c0a80550602de
nthiessen <nick.a.d.thiessen@gmail.com>
2016-02-04 12:45:26 -0600

    added user testing
    added user testing

--- apartment/apartmentApp/tests.py
+++ apartment/apartmentApp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserTestCase(TestCase):
+
+	def test_user_create(self):
+		name = 'Timmy the Tester'
+		user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
+		self.assertTrue(User.objects.get(username=name))
+
+	def test_user_delete(self):
+		name = 'Timmy the Tester'
+		user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
+		User.objects.get(username=name)
+		self.assertTrue(User.objects.get(username=name).delete())		
