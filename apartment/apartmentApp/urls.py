dd65cb31d028e10c7fe481a8861d54b14bdbf1df
nthiessen <nick.a.d.thiessen@gmail.com>
2016-02-04 11:56:03 -0600

    added user creation
    added user creation

--- apartment/apartmentApp/urls.py
+++ apartment/apartmentApp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^home', views.home, name='home'),
 	url(r'^invalidLogin', views.invalidLogin, name='invalidLogin'),
 	url(r'^logoutUser', views.logoutUser, name='logoutUser'),
+    url(r'^createUser', views.createUser, name='createUser'),
 ]
--- apartment/apartmentApp/views.py
+++ apartment/apartmentApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.template import loader
 
@@ -28,4 +29,8 @@ def invalidLogin(request):
 
 def home(request):
     return render(request,'home.html')
+
+def createUser(request):
+    user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+    return render(request, 'home.html')
 
