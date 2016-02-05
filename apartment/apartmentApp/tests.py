546d022e36ac89764acec29466913ef8bfe0aa51
COURAGE PAUL <umeboigb@myumanitoba.ca>
2016-02-05 15:08:09 -0600

    modified user test

--- apartment/apartmentApp/tests.py
+++ apartment/apartmentApp/tests.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 class UserTestCase(TestCase):
 
-	def test_user_create(self):
-		name = 'Timmy the Tester'
-		user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
-		self.assertTrue(User.objects.get(username=name))
+    def test_user_create(self):
+        name = 'Timmy the Tester'
+        user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
+        self.assertTrue(User.objects.get(username=name))
 
-	def test_user_delete(self):
-		name = 'Timmy the Tester'
-		user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
-		User.objects.get(username=name)
-		self.assertTrue(User.objects.get(username=name).delete())
-        
+    def test_user_delete(self):
+        name = 'Timmy the Tester'
+        user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
+        User.objects.get(username=name)
+        self.assertTrue(User.objects.get(username=name).delete())
+
     def test_get_user(self):
         name = 'Timmy the Tester'
         user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
