12fc8df89120d07fcdc3f68c69fe1027b9a135ef
Josh Da Silva <joshdasilva92@gmail.com>
2016-02-05 15:47:23 -0600

    Fixed conflict.

--- apartment/apartmentApp/tests.py
+++ apartment/apartmentApp/tests.py
@@ -7,17 +7,22 @@ import datetime
 
 class UserTestCase(TestCase):
 
-<<<<<<< HEAD
-	def test_user_create(self):
-		name = 'Timmy the Tester'
-		user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
-		self.assertTrue(User.objects.get(username=name))
+    def test_user_create(self):
+        name = 'Timmy the Tester'
+        user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
+        self.assertTrue(User.objects.get(username=name))
 
-	def test_user_delete(self):
-		name = 'Timmy the Tester'
-		user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
-		User.objects.get(username=name)
-		self.assertTrue(User.objects.get(username=name).delete())
+    def test_user_delete(self):
+        name = 'Timmy the Tester'
+        user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
+        User.objects.get(username=name)
+        self.assertTrue(User.objects.get(username=name).delete())
+
+    def test_get_user(self):
+        name = 'Timmy the Tester'
+        user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
+        User.objects.get(username=name)
+        self.assertTrue(User.objects.get(username=name))
 
 class MessageTestCase(TestCase):
 
@@ -60,21 +65,3 @@ class MessageTestCase(TestCase):
         message.save()
         Message.objects.get(message_id = message_id)
         self.assertTrue(Message.objects.get(message_id = message_id).delete())
-=======
-    def test_user_create(self):
-        name = 'Timmy the Tester'
-        user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
-        self.assertTrue(User.objects.get(username=name))
-
-    def test_user_delete(self):
-        name = 'Timmy the Tester'
-        user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
-        User.objects.get(username=name)
-        self.assertTrue(User.objects.get(username=name).delete())
-
-    def test_get_user(self):
-        name = 'Timmy the Tester'
-        user = User.objects.create_user(name, 'blahblah@gmail.com', 'password')
-        User.objects.get(username=name)
-        self.assertTrue(User.objects.get(username=name))
->>>>>>> user
