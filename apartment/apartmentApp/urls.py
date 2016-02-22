d19d0ef81cf7d9e5aa903e6978743341c8586221
nthiessen <nick.a.d.thiessen@gmail.com>
2016-02-22 13:26:26 -0600

    Links
    Added the proper routing to the managerSettings page

--- apartment/apartmentApp/urls.py
+++ apartment/apartmentApp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^home', views.home, name='home'),
 	url(r'^invalidLogin', views.invalidLogin, name='invalidLogin'),
 	url(r'^logoutUser', views.logoutUser, name='logoutUser'),
+	url(r'^managerSettings', views.managerSettings, name='managerSettings'),
     url(r'^createUser', views.createUser, name='createUser'),
 	url(r'^deleteUser', views.deleteUser, name='deleteUser'),
     url(r'^bulletinBoard/$', views.bulletinBoard, name='bulletinBoard'),
