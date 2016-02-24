52c2a2e78cb7d0ffc08fef97d7fe87d674a2cf40
anTIMattter <kedingt@gmail.com>
2016-02-24 01:43:06 -0600

    added code for creating bulletin and comment

--- apartment/bulletin/urls.py
+++ apartment/bulletin/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^bulletinBoard/$', views.bulletinBoard, name='bulletinBoard'),
     url(r'^bulletinBoard/createBulletin', views.createBulletin, name='createBulletin'),
     url(r'^bulletinBoard/(?P<bulletin_id>[0-9]+)', views.bulletin, name='bulletin'),
+    url(r'^bulletinBoard/sendBulletin', views.sendBulletin, name='sendBulletin'),
+    url(r'^bulletinBoard/sendComment', views.sendComment, name='sendComment'),
 ]
--- apartment/bulletin/views.py
+++ apartment/bulletin/views.py
@@ -1,5 +1,6 @@
 import time
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 from lib.dynamo import Dynamo
 
@@ -13,22 +14,25 @@ def sendBulletin(request):
         }
 
         Dynamo.initialize().send_bulletin(bulletin)
+        return redirect(createBulletin)
     except Exception as e:
         print("\tERROR\tFailed to send bulletin: " + str(e))
-        # return redirect(errorMessage)
+        return redirect(error_message)
 
 def sendComment(request):
     try:
         comment = {
             'sender': 'test',
-            'content': str(request.POST('message')),
+            'content': str(request.POST['message']),
             'timestamp': int(time.time())
         }
 
         Dynamo.initialize().send_comment(comment)
+        bulletin = request.POST['bulletin']
+        return redirect(bulletin(bulletin.bulletin_id))
     except Exception as e:
         print("\tERROR\tFailed to send bulletin comment: " + str(e))
-        # return redirect(errorMessage)
+        return redirect(error_message)
 
 
 def bulletinBoard(request):
@@ -39,5 +43,9 @@ def createBulletin(request):
 
 def bulletin(request, bulletin_id):
     return render(request, 'bulletin.html')
+
+def error_message():
+    html = "Error creating message"
+    return HttpResponse(html)
 
 
