1770c6a3533245452e310f1d9db38602c20eb784
Jourey Basco <jbasco77@gmail.com>
2016-02-25 21:39:20 -0600

    Made all tests login before actually executing their code Applied chrome change to all of them, please uncomment firefox and comment the chrome if you are using firefox Got rid of the .close() to stop the chrome replicating bug

--- apartment/apartmentApp/frontTesting.py
+++ apartment/apartmentApp/frontTesting.py
@@ -3,40 +3,39 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+def loginSession(self):
+    #make sure we are in the home page
+    self.driver.get("http://127.0.0.1:8000/apartmentApp/")
+    username = "tester"
+    password = "test"
+    #username
+    name = self.driver.find_element_by_name("username")
+    name.send_keys(Keys.CONTROL + "a")
+    name.send_keys(Keys.DELETE)
+    name.send_keys(username)
+    #password
+    passfield = self.driver.find_element_by_name("password")
+    passfield.send_keys(Keys.CONTROL + "a")
+    passfield.send_keys(Keys.DELETE)
+    passfield.send_keys(password)
+    #submit button
+    submit = '//input[@type="submit" and @value="Login"]'
+    submitButton = self.driver.find_element_by_xpath(submit)
+    submitButton.click()
+
 
 class ApartmentLoginTestCase(unittest.TestCase):
     """Test Case for logging into the app"""
 
     def setUp(self):
-        """
-        if your browser is chrome,change self.driver = webdriver.Firefox() to the follows
+        # if your browser is chrome,change self.driver = webdriver.Firefox() to the follows
         chromedriver = "chromedriver.exe"
         os.environ["webdriver.chrome.driver"] = chromedriver
         self.driver = webdriver.Chrome(chromedriver)
-
-        """
-        # if the browser if Firefox
-        self.driver = webdriver.Firefox()
-        self.driver.get("http://127.0.0.1:8000/apartmentApp/")
+        # self.driver = webdriver.Firefox()
 
     def test_login(self):
-        #make sure we are in the home page
-        username = "jourey"
-        password = "test"
-        #username
-        name = self.driver.find_element_by_name("username")
-        name.send_keys(Keys.CONTROL + "a")
-        name.send_keys(Keys.DELETE)
-        name.send_keys(username)
-        #password
-        passfield = self.driver.find_element_by_name("password")
-        passfield.send_keys(Keys.CONTROL + "a")
-        passfield.send_keys(Keys.DELETE)
-        passfield.send_keys(password)
-        #submit button
-        submit = '//input[@type="submit" and @value="Login"]'
-        submitButton = self.driver.find_element_by_xpath(submit)
-        submitButton.click()
+        loginSession(self)
 
     def tearDown(self):
         self.driver.close()
@@ -44,20 +43,17 @@ class ApartmentLoginTestCase(unittest.TestCase):
 class ApartmentSendMessageTestCase(unittest.TestCase):
     """Test Case for sending messages to a test user"""
     def setUp(self):
-
-        """
-        if your browser is chrome,change self.driver = webdriver.Firefox() to the follows
+        # if your browser is chrome,change self.driver = webdriver.Firefox() to the follows
         chromedriver = "chromedriver.exe"
         os.environ["webdriver.chrome.driver"] = chromedriver
         self.driver = webdriver.Chrome(chromedriver)
-        """
-        # self.driver.get("http://apartment-app.pfsa2harbh.us-west-2.elasticbeanstalk.com/apartmentApp")
-        self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Firefox()
+        loginSession(self)
 
     def test_sending_messages(self):
         #make sure we are in the home page
         # the url should be changed to create message url
-        self.driver.get("file:///Users/caoqing/Desktop/COMP4350/apartment/messaging/templates/createMessage.html")
+        self.driver.get("http://127.0.0.1:8000/apartmentApp/createMessageView")
         username = "jourey"
         msg = "test message from selenium"
         urg = "1"
@@ -74,6 +70,8 @@ class ApartmentSendMessageTestCase(unittest.TestCase):
         send = '//input[@type="submit" and @value="Send"]'
         sendButton = self.driver.find_element_by_xpath(send)
         sendButton.click()
+
+    def tearDown(self):
         self.driver.close()
 
 
@@ -81,59 +79,55 @@ class ApartmentSendMessageTestCase(unittest.TestCase):
 class ApartmentBulletinBoardTestCase(unittest.TestCase):
 
     def setUp(self):
-        """
-        if your browser is chrome,change self.driver = webdriver.Firefox() to the follows
+        # if your browser is chrome,change self.driver = webdriver.Firefox() to the follows
         chromedriver = "chromedriver.exe"
         os.environ["webdriver.chrome.driver"] = chromedriver
         self.driver = webdriver.Chrome(chromedriver)
-        """
-
-        self.driver = webdriver.Firefox()
-
+        # self.driver = webdriver.Firefox()
+        loginSession(self)
 
     def test_jump_from_bulletin_board_to_create_new_bulletin_page(self):
         # url should be changed to bulletin board url
-        self.driver.get("file:///Users/caoqing/Desktop/COMP4350/apartment/bulletin/templates/bulletinBoard.html") #should be the url of bulletin board
+        self.driver.get("http://127.0.0.1:8000/apartmentApp/bulletinBoard")
         create = '//button[text()="New Bulletin"]'
         createButton = self.driver.find_element_by_xpath(create)
         createButton.click()
-        self.driver.close()
 
     def test_create_new_bulletin(self):
         # url should be changed to create bulletin url
-        self.driver.get("file:///Users/caoqing/Desktop/COMP4350/apartment/bulletin/templates/createBulletin.html")
+        self.driver.get("http://127.0.0.1:8000/apartmentApp/bulletinBoard/createBulletin")
         content = self.driver.find_element_by_name("Text1")
         content.send_keys("Attention! This is a test bulletin from selenium")
         create = '//button[text()="Create Bulletin"]'
         createButton = self.driver.find_element_by_xpath(create)
         createButton.click()
-        self.driver.close()
-
 
     def test_add_comment(self):
         # url should be changed to bulletin url
-        self.driver.get("file:///Users/caoqing/Desktop/COMP4350/apartment/bulletin/templates/bulletin.html")
+        # need to double check this test TODO: fix test to work with proper bulletins
+        self.driver.get("http://127.0.0.1:8000/apartmentApp/bulletinBoard/10")
         comment = self.driver.find_element_by_class_name("form-control")
         comment.send_keys("This is a test comment from selenium")
         add = '//button[text()="Add"]'
         addButton = self.driver.find_element_by_xpath(add)
         addButton.click()
+
+    def tearDown(self):
         self.driver.close()
 
 class ManagerSettingTestCase(unittest.TestCase):
 
     def setUp(self):
-        """
-        if your browser is chrome,change self.driver = webdriver.Firefox() to the follows
+        # if your browser is chrome,change self.driver = webdriver.Firefox() to the follows
         chromedriver = "chromedriver.exe"
         os.environ["webdriver.chrome.driver"] = chromedriver
         self.driver = webdriver.Chrome(chromedriver)
-        """
-        self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Firefox()
+        loginSession(self)
 
     def test_create_new_tenant(self):
         # the url should be changed to managerSetting url
-        self.driver.get("file:///Users/caoqing/Desktop/COMP4350/apartment/apartmentApp/templates/managerSettings.html")
+        self.driver.get("http://127.0.0.1:8000/apartmentApp/managerSettings")
         name = self.driver.find_element_by_name("username")
         name.send_keys(Keys.CONTROL,'a')
         name.send_keys(Keys.DELETE)
@@ -149,11 +143,11 @@ class ManagerSettingTestCase(unittest.TestCase):
         create = '//input[@type="submit" and @value="Create Tenant"]'
         createTenant = self.driver.find_element_by_xpath(create)
         createTenant.click()
-        self.driver.close()
 
     def test_delete_tenant(self):
         # the url should be changed to managerSetting url
-        self.driver.get("file:///Users/caoqing/Desktop/COMP4350/apartment/apartmentApp/templates/managerSettings.html")
+        self.driver.get("http://127.0.0.1:8000/apartmentApp/managerSettings")
+        #TODO: Change this to be proper name, asked nick to change it
         name = self.driver.find_element_by_name("username")
         name.send_keys(Keys.CONTROL,'a')
         name.send_keys(Keys.DELETE)
@@ -161,6 +155,8 @@ class ManagerSettingTestCase(unittest.TestCase):
         remove = '//input[@type="submit" and @value="Remove Tenant"]'
         removeTenant = self.driver.find_element_by_xpath(remove)
         removeTenant.click()
+
+    def tearDown(self):
         self.driver.close()
 
 #run the tests
